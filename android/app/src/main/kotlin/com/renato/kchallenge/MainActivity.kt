d9df67040070e18375c510cb85344dbbbe300be7
Renato Montufar <52421382+deus-magna@users.noreply.github.com>
2025-03-22 17:23:54 -0600

    first commit

---
+++ android/app/src/main/kotlin/com/renato/kchallenge/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.renato.kchallenge
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_development.dart
@@ -0,0 +1,6 @@
+import 'package:kueski_challenge/app/app.dart';
+import 'package:kueski_challenge/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
