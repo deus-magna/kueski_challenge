615fc1d0c5b7cc5273a9e10b5f05989e1c738080
Renato Montufar <52421382+deus-magna@users.noreply.github.com>
2025-03-22 17:41:54 -0600

    feat: added dependencies rules

---
+++ lib/injection_container.dart
@@ -0,0 +1,24 @@
+import 'package:dio/dio.dart';
+import 'package:get_it/get_it.dart';
+import 'package:kueski_challenge/data/adapters/movies_adapter.dart';
+import 'package:kueski_challenge/data/datasources/movies_remote_datasource.dart';
+import 'package:kueski_challenge/domain/repositories/movies_repo.dart';
+
+final sl = GetIt.instance;
+
+/// Init the dependency injection rules
+Future<void> init() async {
+  sl
+    // Repos
+    ..registerLazySingleton<MoviesRepo>(
+      () => MoviesAdapter(
+        moviesRemoteDataSource: sl<MoviesRemoteDataSource>(),
+      ),
+    )
+    // Datasources
+    ..registerLazySingleton<MoviesRemoteDataSource>(
+      () => MoviesRemoteDataSourceImpl(dio: sl<Dio>()),
+    )
+    // Core
+    ..registerLazySingleton(Dio.new);
+}
