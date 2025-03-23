89aeb6bdc5726b22ac9748fa90518b1679940634
Renato Montufar <52421382+deus-magna@users.noreply.github.com>
2025-03-23 13:52:01 -0600

    feat: added selected movie indicator

--- lib/presentation/widgets/movies_page_view.dart
+++ lib/presentation/widgets/movies_page_view.dart
@@ -3,8 +3,8 @@ import 'package:flutter/material.dart';
 import 'package:kueski_challenge/domain/entities/movies_response.dart';
 import 'package:shimmer/shimmer.dart';
 
-class MoviesPageView extends StatelessWidget {
-  MoviesPageView({
+class MoviesPageView extends StatefulWidget {
+  const MoviesPageView({
     required this.movies,
     required this.nextPage,
     required this.onPageChanged,
@@ -15,40 +15,59 @@ class MoviesPageView extends StatelessWidget {
   final void Function() nextPage;
   final void Function(Movie) onPageChanged;
 
+  @override
+  State<MoviesPageView> createState() => _MoviesPageViewState();
+}
+
+class _MoviesPageViewState extends State<MoviesPageView> {
   final _pageController = PageController(
-    initialPage: 1,
     viewportFraction: 0.4,
   );
+  int _selectedIndex = 0;
+
+  @override
+  void initState() {
+    _pageController.addListener(() {
+      if (_pageController.position.pixels ==
+          _pageController.position.maxScrollExtent) {
+        widget.nextPage();
+      }
+    });
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
     final screenSize = MediaQuery.of(context).size;
 
-    _pageController.addListener(() {
-      if (_pageController.position.pixels ==
-          _pageController.position.maxScrollExtent) {
-        print('next page');
-        nextPage();
-      }
-    });
-
     return SizedBox(
       height: screenSize.height * 0.3,
       child: PageView.builder(
-        pageSnapping: false,
         controller: _pageController,
-        itemCount: movies.length,
-        itemBuilder: (context, index) => _MovieCard(movie: movies[index]),
-        onPageChanged: (value) => onPageChanged(movies[value]),
+        itemCount: widget.movies.length,
+        itemBuilder: (context, index) => _MovieCard(
+          movie: widget.movies[index],
+          isSelected: index == _selectedIndex,
+        ),
+        onPageChanged: (value) {
+          if (value < widget.movies.length) {
+            _selectedIndex = value;
+            widget.onPageChanged(widget.movies[value]);
+          }
+        },
       ),
     );
   }
 }
 
 class _MovieCard extends StatelessWidget {
-  const _MovieCard({required this.movie});
+  const _MovieCard({
+    required this.movie,
+    required this.isSelected,
+  });
 
   final Movie movie;
+  final bool isSelected;
 
   @override
   Widget build(BuildContext context) {
@@ -56,31 +75,40 @@ class _MovieCard extends StatelessWidget {
       margin: const EdgeInsets.only(right: 15),
       child: Column(
         children: <Widget>[
-          ClipRRect(
-            borderRadius: BorderRadius.circular(20),
-            child: CachedNetworkImage(
-              imageUrl: movie.getPosterImg(),
-              fit: BoxFit.cover,
-              height: 200,
-              progressIndicatorBuilder: (context, url, progress) {
-                return Shimmer.fromColors(
-                  baseColor: Colors.grey[300]!,
-                  highlightColor: Colors.grey[100]!,
-                  child: Container(
-                    height: 200,
-                    width: 135,
-                    color: Colors.white,
-                  ),
-                );
-              },
+          Container(
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(20),
+              border: Border.all(
+                width: 2,
+                color: isSelected ? Colors.orange : Colors.transparent,
+              ),
+            ),
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(18),
+              child: CachedNetworkImage(
+                imageUrl: movie.getPosterImg(),
+                fit: BoxFit.cover,
+                height: 200,
+                progressIndicatorBuilder: (context, url, progress) {
+                  return Shimmer.fromColors(
+                    baseColor: Colors.grey[300]!,
+                    highlightColor: Colors.grey[100]!,
+                    child: Container(
+                      height: 200,
+                      width: 135,
+                      color: Colors.white,
+                    ),
+                  );
+                },
+              ),
             ),
           ),
           const SizedBox(height: 5),
           Text(
             movie.title,
             overflow: TextOverflow.ellipsis,
-            style: const TextStyle(
-              color: Colors.white,
+            style: TextStyle(
+              color: isSelected ? Colors.orange : Colors.white,
               fontWeight: FontWeight.bold,
               fontSize: 12,
             ),
